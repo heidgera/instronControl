d1a53ed2fb857bded6b0a8e94c0739e60baba2e2
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-10-10 15:34:57 -0500

    moving computers

--- local/src/app.js
+++ local/src/app.js
@@ -1,14 +1,28 @@
 'use strict';
 
-obtain(['./src/server/express.js'], (hw)=> {
+obtain(['µ/serial.js'], ({ Serial })=> {
   exports.app = {};
+
+  var serial = new Serial();
+
+  serial.open('usbserial', 9600);
+
+  serial.onMessage = (data)=> {
+    var pol = (data[6] == '+') ? 1 : -1;
+    console.log(pol * parseFloat(data.substring(7, 14)));
+    var units = data.substr(14, 15);
+  };
+
+  setInterval(()=> {
+    serial.write('R');
+    //console.log('Sent!');
+  }, 500);
 
   exports.app.start = ()=> {
     console.log('started');
 
     document.onkeypress = (e)=> {
       if (e.key == ' ') console.log('Space pressed');
-      else if
     };
 
     document.onkeyup = (e)=> {
