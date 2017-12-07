647c413c0c3e4a41a0127661459184ffab35b3c3
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-12-07 09:31:52 -0600

    testing motor

--- local/src/backend/index.js
+++ local/src/backend/index.js
@@ -1,10 +1,10 @@
 var obtains = [
   './src/backend/encoder.js',
   './src/backend/loadCell.js',
-  './src/backend/motorDriver.js'
+  './src/backend/motorDriver.js',
 ];
 
-obtain(, ({ Encoder }, { Scale }, {Driver})=> {
+obtain(obtains, ({ Encoder }, { Scale }, { Driver })=> {
   exports.Encoder = Encoder;
   exports.Scale = Scale;
   exports.Driver = Driver;
