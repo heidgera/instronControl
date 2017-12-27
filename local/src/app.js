66f71bb9f5e1da39efcf8a84133fbe4026a751dc
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-12-27 08:40:19 -0600

    fixes for wifi

--- local/src/app.js
+++ local/src/app.js
@@ -2,11 +2,10 @@
 
 var obtains = [
   'µ/components',
-  'os',
   `./src/interface/wifiConfig/wifiControl.js`,
 ];
 
-obtain(obtains, ({ Button, Dropdown, Card, Menu }, os, wifi)=> {
+obtain(obtains, ({ Button, Dropdown, Card, Menu }, wifi)=> {
   exports.app = {};
 
   //var encoder = new Encoder(17, 27);
@@ -20,6 +19,13 @@ obtain(obtains, ({ Button, Dropdown, Card, Menu }, os, wifi)=> {
     console.log(`New value is ${scale.value}`);
   };*/
 
+  wifi.scan((err, networks)=> {
+    if (!err) {
+      console.log('--------------------');
+      console.log(networks);
+    }
+  });
+
   exports.app.start = ()=> {
     if (process.platform == 'darwin') {
       //µ('.rotator')[0].className = 'normal';
@@ -29,123 +35,6 @@ obtain(obtains, ({ Button, Dropdown, Card, Menu }, os, wifi)=> {
 
     document.onkeydown = (e)=> {
       if (e.key == ' ') console.log('Space pressed');
-    };
-
-    µ('#wifiConfig').onready = ()=> {
-      var setWifiOptsStartPos = ()=> {
-        var rect = µ('#wifiIcon').getBoundingClientRect();
-        µ('#wifiOpts').style.removeProperty('--button-pos-x');
-        µ('#wifiOpts').style.setProperty('--button-pos-x', (window.innerWidth - rect.left) + 'px');
-        µ('#wifiOpts').style.removeProperty('--button-pos-y');
-        µ('#wifiOpts').style.setProperty('--button-pos-y', rect.bottom + 'px');
-      };
-
-      µ('input', µ('#wifiConfig')).forEach((el)=> {
-        var checkClick = (e)=> {
-          if (e.target != document.activeElement && e.target.parentElement.id != 'keyboard')
-            el.blur();
-        };
-
-        el.onclick = ()=> {
-          el.focus();
-        };
-
-        el.onfocus = ()=> {
-          µ('#keyboardDiv').classList.add('show');
-          document.addEventListener('click', checkClick);
-        };
-
-        el.onblur = ()=> {
-          µ('#keyboardDiv').classList.remove('show');
-          document.removeEventListener('click', checkClick);
-        };
-      });
-
-      µ('#wifiConfig').onready = ()=> {
-        var checkWifiConnection = ()=> {
-          var interfaces = os.networkInterfaces();
-          for (var k in interfaces) {
-            for (var k2 in interfaces[k]) {
-              var address = interfaces[k][k2];
-              if (address.family === 'IPv4' && !address.internal) {
-                µ('#question').style.display = 'none';
-                µ('#wifiIcon').style.color = 'currentColor';
-              }
-            }
-          }
-        };
-
-        checkWifiConnection();
-
-        µ('#ssids').disabled = true;
-
-        wifi.scan((err, networks)=> {
-          console.log(networks);
-          if (err) {
-            console.log(err);
-          } else {
-            µ('#ssids').disabled = false;
-            µ('#ssids').default = 'Choose a network';
-            µ('#ssids').innerHTML = '';
-            networks.forEach((ntwk, ind)=> {
-              if (µ(`[value="${ntwk}"]`, µ('#ssids')).length == 0) {
-                let newOpt = µ('+drop-opt', µ('#ssids'));
-                newOpt.textContent = ntwk;
-                newOpt.value = ntwk;
-              }
-            });
-          }
-        });
-
-        µ('#wifiPass').onclick = ()=> {
-          µ('#wifiPass').select();
-        };
-
-        µ('#accept').onclick = ()=> {
-          if (!µ('#ssids').value) {
-            µ('#growl').message('Please select an Access Point', 'warn');
-            return;
-          }
-
-          let loading = µ('+div', µ('body')[0]);
-          loading.className = 'loadingOverlay';
-          loading.textContent = 'Loading...';
-          wifi.connect({ ssid: µ('#ssids').value, password: µ('#wifiPass').value }, function (err) {
-            if (err) {
-              console.log(err);
-              return;
-            }
-
-            µ('#growl').message(`Connected to ${µ('#ssids').value}`, 'success');
-            loading.parentElement.removeChild(loading);
-          });
-
-          µ('#wifiOpts').opened = false;
-        };
-
-        µ('#cancel').onclick = ()=> µ('#wifiOpts').opened = false;
-
-        µ('#wifiIcon').onclick = (e)=> {
-          e.stopPropagation();
-          //setWifiOptsStartPos();
-          µ('#wifiOpts').opened = !µ('muse-card')[0].opened;
-        };
-
-        µ('#wifiOpts').makeTransitionState('opened', 'closed');
-
-        µ('#wifiOpts').onOpened = ()=> {
-          µ('#wifiIcon').classList.add('opened');
-        };
-
-        µ('#wifiOpts').onClickOutsideCard = (e)=> {
-          if (e.target.parentElement.id != 'keyboard')
-            if (µ('#wifiOpts').opened) µ('#wifiOpts').opened = false;
-        };
-
-        µ('#wifiOpts').onClosed = ()=> {
-          µ('#wifiIcon').classList.remove('opened');
-        };
-      };
     };
 
     document.onkeyup = (e)=> {
